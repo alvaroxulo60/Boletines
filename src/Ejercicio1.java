34261bd8fd0692d8d26e8813f61bee80bda05e02
Álvaro <alvaroxulo60@gmail.com>
2025-09-23 12:47:54 +0200

    prueba

---
+++ src/Ejercicio1.java
@@ -0,0 +1,12 @@
+public class Ejercicio1 {
+private int n1 = 0;
+private int multiplos7 = 0;
+private int multiplos13 = 0;
+    public static void main(String[] args) {
+        n1 = i;
+        for (int i>0; i<100; i++);
+        if(i % 7 == 0);
+        multiplos7++;
+
+    }
+}
