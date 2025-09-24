57882b4af1a4adbf189adb60a845cbd247ee3b7c
Álvaro <alvaroxulo60@gmail.com>
2025-09-24 10:19:10 +0200

    Ejercicios

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -5,6 +5,10 @@ public class Ejercicio2 {
         System.out.println("Introduce un numero: ");
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
+        while (n1 < 0 || n1 > 10) {
+            System.out.println("Introduce un numero entre el 0 y el 10");
+            n1 = sc.nextInt();
+        }
         for (int i = 0; i < 11; i++) {
             System.out.println(n1 + "*" + i + "=" + n1 * i);
         }
--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -4,11 +4,21 @@ public class Ejercicio3 {
     public static void main(String[] args) {
         System.out.println("¿Cuantos numeros quieres introducir?");
         Scanner sc = new Scanner(System.in);
-        int cant = 0;
-        cant = sc.nextInt();
-        while (cant == 0 || cant<0 ){
-        System.out.println("Introduzca una cantidad valida: ");
-        cant = sc.nextInt();
+        int cant = sc.nextInt();
+        while (cant == 0 || cant < 0) {
+            System.out.println("Introduzca una cantidad valida: ");
+            cant = sc.nextInt();
+        }
+        for (int i = 1; i <= cant; i++) {
+            System.out.println("Introduzca su " + i + " numero");
+            int num = sc.nextInt();
+            if (num % 2 == 0) {
+                System.out.println("El numero " + num + " Es par");
+            }
+            else
+                System.out.println("El numero " + num + " es impar");
+
         }
     }
 }
+
