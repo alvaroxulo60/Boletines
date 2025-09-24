6e901189e175f3987fcadde0cb4b221eab4d1b0d
Álvaro <alvaroxulo60@gmail.com>
2025-09-24 08:43:21 +0200

    Ejercicios

---
+++ src/Ejercicio2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Introduce un numero: ");
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+
+        for (int i = 0; i <11 ; i++) {
+            System.out.println(n1+" * "+ i +" = " +n1*i );
+        }
+
+
+    }
+}
