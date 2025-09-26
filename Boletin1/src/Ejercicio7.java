fee35ada515b4834ac933698bbe467453d8e6223
Álvaro <alvaroxulo60@gmail.com>
2025-09-26 08:27:48 +0200

    estructuraccion

--- Boletin1/src/Ejercicio7.java
+++ Boletin1/src/Ejercicio7.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class Ejercicio7 {
     public static void main(String[] args) {
-        System.out.println("Introduzca la cantidad de numeros que va a introducir: ");
+        System.out.println("Introduzca la cantidad de números que va a introducir: ");
         Scanner sc = new Scanner(System.in);
         int cant = sc.nextInt();
         double suma = 0;
         for (int i = 1; i <= cant; i++) {
-            System.out.println("Introduce el " + i + "º numero");
+            System.out.println("Introduce el " + i + "º número");
             int num = sc.nextInt();
             suma += num;
         }
         double media = suma / cant;
-        System.out.println("La media de esos numeros es " + media);
+        System.out.println("La media de esos números es " + media);
     }
 }
