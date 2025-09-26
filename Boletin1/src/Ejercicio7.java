c427366c217ecf5e5b5c08f09530a9534700da3e
Álvaro <alvaroxulo60@gmail.com>
2025-09-26 10:08:25 +0200

    estructuraccion

--- Boletin1/src/Ejercicio7.java
+++ Boletin1/src/Ejercicio7.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class Ejercicio7 {
     public static void main(String[] args) {
+
         System.out.println("Introduzca la cantidad de números que va a introducir: ");
         Scanner sc = new Scanner(System.in);
         int cant = sc.nextInt();
         double suma = 0;
+        while (cant<=0){
+            System.out.println("Esa cantidad no es valida. Introduce una nueva: ");
+            cant = sc.nextInt();
+        }
         for (int i = 1; i <= cant; i++) {
             System.out.println("Introduce el " + i + "º número");
             int num = sc.nextInt();
