2ee2e299d1ccc142de76586c87be690b98b61c64
Álvaro <alvaroxulo60@gmail.com>
2025-09-30 11:15:45 +0200

    estructuraccion

--- Boletin1/src/Ejercicio6.java
+++ Boletin1/src/Ejercicio6.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Ejercicio6 {
     public static void main(String[] args) {
-        System.out.println("Introduce tu primer numero: ");
         Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce tu primer numero: ");
         int n1 = sc.nextInt();
-        int res = 0;
         System.out.println("Introduce tu segundo numero: ");
         int n2 = sc.nextInt();
-        for (int i = 0; i < n2; i++) {
+        int res = 0;
+        for (int i = 0; i < Math.abs(n2); i++) {
             res += n1;
         }
         System.out.println("El resultado del producto es: " + res);
