5be30848e37745ff76e0e07722d0bdfa63b53602
Álvaro <alvaroxulo60@gmail.com>
2025-10-01 09:13:11 +0200

    boletin 2

---
+++ Boletin2/src/Ejercicio1.java
@@ -0,0 +1,7 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Introduce tus 3 numeros:");
+        }
+    }
+}
