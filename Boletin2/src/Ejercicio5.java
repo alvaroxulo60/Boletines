e646b8ab7da8bbd98a6ed2131592a8508b31d6af
Álvaro <alvaroxulo60@gmail.com>
2025-10-01 13:10:10 +0200

    boletin 2

--- Boletin2/src/Ejercicio5.java
+++ Boletin2/src/Ejercicio5.java
@@ -7,11 +7,8 @@ public class Ejercicio5 {
                 divisores++;
             }
         }
-        if (divisores ==2 && n1 != 2){
+        if (divisores ==2){
             System.out.printf("El numero %d es primo",n1);
-        }
-        if (n1 == 2){
-            System.out.println("El numero 2 no es primo");
         }
         else
             System.out.printf("El numero %d no es primo",n1);
