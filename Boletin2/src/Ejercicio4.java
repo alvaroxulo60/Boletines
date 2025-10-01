44d4446835e5a7dc09f80c666b6749788e8023e9
Álvaro <alvaroxulo60@gmail.com>
2025-10-01 12:32:27 +0200

    boletin 2

---
+++ Boletin2/src/Ejercicio4.java
@@ -0,0 +1,30 @@
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        double random = Math.random() * 100;
+        int secreto = (int) random;
+        int n1;
+        int error = 1;
+        do {
+            n1 = MiEntradaSalida.leerEnteroPositivo("Introduce un numero entre 1 y 100: ", false);
+            if(n1<0||n1>100){
+                error++;
+            }
+        } while (n1 < 0 || n1 > 100);
+        while (secreto != n1 && error != 5) {
+            if (secreto < n1) {
+                System.out.println("El numero secreto es mas pequeño");
+                n1 = MiEntradaSalida.leerEnteroPositivo("Introduce un numero entre 1 y 100: ", false);
+            } else {
+                System.out.println("El numero secreto es mas grande");
+                n1 = MiEntradaSalida.leerEnteroPositivo("Introduce un numero entre 1 y 100: ", false);
+            }
+            error++;
+        }
+        if (secreto == n1) {
+            System.out.println("Has acertado. Felicidades");
+        }
+        if (error == 5) {
+            System.out.println("Has fallado, intentalo de nuevo. El numero secreto era " + secreto);
+        }
+    }
+}
