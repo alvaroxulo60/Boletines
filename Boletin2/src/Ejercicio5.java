4d23a241831ec914417345c4a5e86561a879c6c4
Álvaro <alvaroxulo60@gmail.com>
2025-10-03 11:58:01 +0200

    boletin 2

--- Boletin2/src/Ejercicio5.java
+++ Boletin2/src/Ejercicio5.java
@@ -1,17 +1,20 @@
 public class Ejercicio5 {
     public static void main(String[] args) {
         int n1 = MiEntradaSalida.leerEnteroPositivo("Introduce tu numero: ", false);
-        int divisores = 0;
         boolean esPrimo = true;
-        for (int i = 2; i <= n1/2&& esPrimo; i++) {
-            if (n1 % i == 0) {
-                divisores++;
+
+        if (n1 <= 1) {
+            esPrimo = false;
+        } else {
+            for (int i = 2; i <= n1 / 2 && esPrimo; i++) {
+                if (n1 % i == 0) {
+                    esPrimo = false;
+                }
             }
         }
-        if (divisores ==0){
-            System.out.printf("El numero %d es primo",n1);
-        }
-        else
-            System.out.printf("El numero %d no es primo",n1);
+        if (esPrimo) {
+            System.out.printf("El numero %d es primo", n1);
+        } else
+            System.out.printf("El numero %d no es primo", n1);
     }
 }
