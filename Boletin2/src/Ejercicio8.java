b4c8633ef3e07688d18a14ec3bfc8580789b8557
Álvaro <alvaroxulo60@gmail.com>
2025-10-08 08:43:41 +0200

    boletin 2

--- Boletin2/src/Ejercicio8.java
+++ Boletin2/src/Ejercicio8.java
@@ -2,7 +2,7 @@ public class Ejercicio8 {
     public static void main(String[] args) {
         double a = MiEntradaSalida.leerEntero("Introduce tu primer numero: ");
         double b = MiEntradaSalida.leerEntero("Introduce tu segundo numero: ");
-        double c = MiEntradaSalida.leerEntero("Introduce tu segundo numero: ");
+        double c = MiEntradaSalida.leerEntero("Introduce tu tercer numero: ");
         System.out.println("Esta ecuación de segundo grado tiene "+MiEntradaSalida.ecuacionSegundoGrado(a,b,c, false)+" soluciones");
     }
 }
