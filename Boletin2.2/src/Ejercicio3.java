ae6150d086a349798fdbfefa083bf4ec5d4fc7a3
Álvaro <alvaroxulo60@gmail.com>
2025-10-09 12:03:52 +0200

    Boletin2.2

--- Boletin2.2/src/Ejercicio3.java
+++ Boletin2.2/src/Ejercicio3.java
@@ -1,35 +1,78 @@
 public class Ejercicio3 {
-    public static final int M = 1000, C = 100, I = 1, V = 5, X = 10, L = 50, D = 600;
-
     public static void main(String[] args) {
-        int resultado = 0;
-        int temporal = Integer.MAX_VALUE;
-        String numero = MiEntradaSalida.leerTexto("Introduce el número romano: ");
-        for (int i = 0; i < numero.length(); i++) {
-            char a = numero.charAt(i);
-            if (resultado == 0) {
-                resultado = valorDeLetra(a);
-            } else if (resultado < valorDeLetra(a)) {
-                resultado = valorDeLetra(a) - resultado;
-            } else {
-                resultado += valorDeLetra(a);
+        final int M = 1000;
+        final int D = 500;
+        final int C = 100;
+        final int L = 50;
+        final int X = 10;
+        final int V = 5;
+        final int I = 1;
+        String numRomano;
+        int acumulado = 0;
+        int temporal = 0;
+        int anterior = Integer.MAX_VALUE;
+        numRomano = MiEntradaSalida.leerTexto("Introduce el numero Romano: ");
+
+
+        for (int i = 0; i < numRomano.length(); i++) {
+            char letraActual = numRomano.charAt(i);
+            int valorDeLetra = 0;
+
+
+            switch (letraActual) {
+                case 'M': {
+                    valorDeLetra = M;
+                    break;
+                }
+                case 'D': {
+                    valorDeLetra = D;
+                    break;
+                }
+                case 'C': {
+                    valorDeLetra = C;
+                    break;
+                }
+                case 'L': {
+                    valorDeLetra = L;
+                    break;
+                }
+                case 'X': {
+                    valorDeLetra = X;
+                    break;
+                }
+                case 'V': {
+                    valorDeLetra = V;
+                    break;
+                }
+                case 'I': {
+                    valorDeLetra = I;
+                    break;
+                }
             }
+
+
+            if (valorDeLetra < anterior) {
+                if (temporal > 0) {
+
+                    acumulado += temporal;
+                }
+
+                temporal = valorDeLetra;
+            } else if (valorDeLetra == anterior) {
+
+                acumulado += temporal + valorDeLetra;
+                temporal = 0;
+            } else {
+                acumulado += valorDeLetra - temporal;
+                temporal = 0;
+            }
+            anterior = valorDeLetra;
         }
-        System.out.println(resultado);
-    }
+        if (temporal > 0) {
+            acumulado += temporal;
+        }
 
-    public static int valorDeLetra(char c) {
-        return switch (c) {
-            case 'M' -> M;
-            case 'C' -> C;
-            case 'I' -> I;
-            case 'V' -> V;
-            case 'X' -> X;
-            case 'L' -> L;
-            case 'D' -> D;
-            default -> 0;
-        };
-    }
 
+        System.out.printf("El número %s en decimal es %d", numRomano, acumulado);
+    }
 }
-
---
+++ Boletin2.2/src/Ejercicio4.java
@@ -0,0 +1,43 @@
+public class Ejercicio4 {
+    public static void main(String[] args) {
+
+
+
+
+        int hora = MiEntradaSalida.leerEnteroPositivo("Introdcuce tu hora: ", true);
+        int minutos = MiEntradaSalida.leerEnteroPositivo("Introdcuce tus minutos: ", true);
+        int segundos = MiEntradaSalida.leerEnteroPositivo("Introdcuce tus segundos: ", true);
+
+
+
+        int sumaHoras = MiEntradaSalida.leerEnteroPositivo("¿Cuantas horas quieres sumar? ", true);
+        int sumaMinutos = MiEntradaSalida.leerEnteroPositivo("¿Cuantos minutos quieres sumar? ", true);
+        int sumaSegundos = MiEntradaSalida.leerEnteroPositivo("¿Cuantos segundos quieres sumar? ", true);
+
+
+        segundos += sumaSegundos;
+        minutos += segundos / 60;
+        segundos = segundos % 60;
+
+
+        minutos += sumaMinutos;
+        hora += minutos / 60;
+        minutos = minutos % 60;
+
+
+        hora += sumaHoras;
+        boolean cambioDia = false;
+        if (hora >= 24) {
+            hora = hora % 24;
+            cambioDia = true;
+        }
+
+
+        System.out.printf("La hora resultante es: %02d:%02d:%02d", hora, minutos, segundos);
+        if (cambioDia) {
+            System.out.print(" (del día siguiente)");
+        }
+        System.out.println();
+    }
+}
+
