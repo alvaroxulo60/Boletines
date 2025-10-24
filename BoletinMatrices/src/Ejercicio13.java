a91f63700a530533e3d6d0a3948243e959b7a4d6
Álvaro <alvaroxulo60@gmail.com>
2025-10-24 10:18:44 +0200

    Boletín Matrices

--- BoletinMatrices/src/Ejercicio13.java
+++ BoletinMatrices/src/Ejercicio13.java
@@ -1,7 +1,7 @@
-public class Ejercicio12 {
-    // Crea un metodo que transforme una matriz CUADRADA (tienes que realizar dicha
+public class Ejercicio13 {
+    //Crea un metodo que transforme una matriz CUADRADA (tienes que realizar dicha
     //comprobación) que recibe por parámetro, en otra, que debe ser su reflejo en diagonal (de
-    //arriba a la izquierda a abajo a la derecha):
+    //abajo a la derecha a arriba a la izquierda):
     public static void main(String[] args) {
         int[][] matriz = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 0}, {3, 2, 1, 4, 5}, {9, 5, 8, 6, 7}, {0, 9, 8, 5, 4}};
         System.out.println("Matriz original: ");
@@ -11,21 +11,22 @@ public class Ejercicio12 {
         MiEntradaSalida.imprimirMatriz(reflejarMatriz(matriz));
     }
 
-    public static int[][] reflejarMatriz(int[][] matriz) {
+    public static int[][] reflejarMatriz(int[][] matriz){
         int[][] reflejada;
         int[][] error = {{-1}};
-        for (int i = 0; i < matriz.length; i++) {
-            if (matriz.length != matriz[i].length) {
+        for (int[] ints : matriz) {
+            if (matriz.length != ints.length) {
                 return error;
             }
         }
         reflejada = new int[matriz.length][matriz[0].length];
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                if (i == j) {
-                    reflejada[i][j] = matriz[i][j];
-                } else {
-                    reflejada[j][i] = matriz[i][j];
+        for (int i = matriz.length-1; i>=0; i--) {
+            for (int j = matriz[i].length-1; j >=0 ; j--) {
+                if (i==j){
+                    reflejada[i][j]=matriz[i][j];
+                }
+                else {
+                    reflejada[j][i]=matriz[i][j];
                 }
             }
         }
