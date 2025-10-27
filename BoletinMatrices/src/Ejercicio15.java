1cc73dbc859925f83f9de7c9ec65be533598b1a6
Álvaro <alvaroxulo60@gmail.com>
2025-10-27 08:25:31 +0100

    Boletín Matrices

--- BoletinMatrices/src/Ejercicio15.java
+++ BoletinMatrices/src/Ejercicio15.java
@@ -1,7 +1,19 @@
 public class Ejercicio15 {
     public static void main(String[] args) {
-        int i= MiEntradaSalida.leerEntero("Introduce la primera cordenada de una matriz");
-        int j = MiEntradaSalida.leerEntero("Introduzca la segunda cordenada");
-        System.out.println("La posicion que ocupa en la matriz las casilla de las cordenadas "+"["+i+","+j+"] es la ");
+        int i= MiEntradaSalida.leerEntero("Introduce la primera cordenada de una matriz 6x6: ");
+        int j = MiEntradaSalida.leerEntero("Introduzca la segunda cordenada de la matriz 6x6: ");
+        int[][] matriz = new int[6][6];
+        int num =1;
+        for (int k = 0; k < 6; k++) {
+            for (int l = 0; l < 6 ; l++) {
+             matriz[k][l]=num++;
+            }
+        }
+        MiEntradaSalida.imprimirMatriz(matriz);
+        System.out.println("La posicion que ocupa en la matriz las casilla de las cordenadas "+"["+i+","+j+"] es la "+ numCasillas(i,j));
+    }
+
+    public static int numCasillas(int i, int j){
+        return (i+1)*(j+1);
     }
 }
