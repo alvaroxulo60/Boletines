314e1e98f419fe9ae4cff44d414c082f46ae28ec
Álvaro <alvaroxulo60@gmail.com>
2025-12-02 10:19:40 +0100

    Boletín objetos

---
+++ BoletinObjetos/src/boletin4_2/Ejercicio8/Partido.java
@@ -0,0 +1,70 @@
+package boletin4_2.Ejercicio8;
+
+import exceptions.PartidoException;
+
+import java.text.ParseException;
+
+public class Partido {
+    private Equipo equipoLocal;
+    private Equipo equipoVisitante;
+    private int jornada;
+    private int golesEquipoLocal;
+    private int golesEquipoVisitante;
+    private boolean jugado;
+    private char quiniela;
+
+
+    public Partido(Equipo equipoLocal, Equipo equipoVisitante, int jornada, int golesEquipoLocal,
+                   int golesEquipoVisitante, boolean jugado, char quiniela) {
+        this.equipoLocal = equipoLocal;
+        this.equipoVisitante = equipoVisitante;
+        this.jornada = jornada;
+        this.golesEquipoLocal = golesEquipoLocal;
+        this.golesEquipoVisitante = golesEquipoVisitante;
+        this.jugado = jugado;
+        this.quiniela = quiniela;
+    }
+
+    public void ponerResultado(String resultado) throws PartidoException {
+        String[] goles = resultado.split("-",2);
+        int[] numGoles = new int[] {
+                Integer.parseInt(goles[0]),
+                Integer.parseInt(goles[1])
+        };
+        if (numGoles[0]<numGoles[1]){
+            this.quiniela='1';
+            golesEquipoLocal=numGoles[0];
+            golesEquipoVisitante=numGoles[1];
+            jugado=true;
+            equipoLocal.sumarPartidoGanados(1);
+        }
+        else if (numGoles[0]==numGoles[1]){
+            this.quiniela='X';
+            golesEquipoLocal=numGoles[0];
+            golesEquipoVisitante=numGoles[1];
+            jugado=true;
+        }else {
+            this.quiniela='2';
+            golesEquipoLocal=numGoles[0];
+            golesEquipoVisitante=numGoles[1];
+            jugado=true;
+            equipoVisitante.sumarPartidoGanados(1);
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (!jugado) {
+            return "Partido{" +
+                    "Partido entre equipo local" + equipoLocal.getNombre() +
+                    "y equipo visitante" + equipoVisitante.getNombre() +
+                    "todavía no se ha jugado”\n";
+        }else
+            return "Partido{" +
+                    "Partido entre equipo local" + equipoLocal.getNombre() +
+                    "y equipo visitante" + equipoVisitante.getNombre() +
+                    "jugado en el estadio" +
+                    " \n";
+    }
+}
+
