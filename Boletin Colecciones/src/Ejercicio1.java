9f97002d3939bfc60ecbc3696ee3970e854d78ab
Álvaro <alvaroxulo60@gmail.com>
2026-02-11 10:02:23 +0100

    MAMAMAMAMA

---
+++ Boletin Colecciones/src/Ejercicio1.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+public class Ejercicio1 {
+
+
+    static void main(String[] args) {
+        Collection<String> nombres = new ArrayList<>();
+
+        nombres.add("Jose");
+        nombres.add("Juan");
+        nombres.add("Rosa");
+
+        for (String n : nombres){
+            System.out.println(n);
+        }
+        System.out.println();
+
+        if(nombres.contains("Juan")){
+            nombres.remove("Juan");
+        }
+
+        for (String n : nombres){
+            System.out.println(n);
+        }
+    }
+}
