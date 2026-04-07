6858f790f99f9b1635fe5e9bbc1609534a36a061
Álvaro <alvaroxulo60@gmail.com>
2026-04-07 13:00:45 +0200

    ejercicio 11 terminado

--- BoletinFicheros/src/ejercicio11/Main.java
+++ BoletinFicheros/src/ejercicio11/Main.java
@@ -5,21 +5,23 @@ import exceptions.Ejercicio11Exception;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Main {
     static void main(String[] args) {
-
+        try {
+            crearDirectorios();
+        } catch (Ejercicio11Exception e) {
+            System.out.printf("%s \n",e.getMessage());
+        }
     }
 
     public static void crearDirectorios() throws Ejercicio11Exception {
 
-        Pattern patron = Pattern.compile("^(?<nombre>\\p{Lu}\\p{Lu}+)\\s(?<apellido1>\\p{Lu}\\p{Lu}+)\\s(?<apellido2>\\p{Lu}\\p{Lu}+)\\s(?<curso>[1-6]º[A-Z]+)$");
+        Pattern patron = Pattern.compile("^(?<nombre>\\p{Lu}\\p{Ll}+)\\s(?<apellido1>\\p{Lu}\\p{Ll}+)\\s(?<apellido2>\\p{Lu}\\p{Ll}+)\\s(?<curso>[1-6]º[A-Z]+)$");
 
-        Path rutaSalida = Path.of("src/ejercicio11/salida");
         Path rutaArchivoContenido = Path.of("src/ejercicio11/Nombres.txt");
 
         if (Files.notExists(rutaArchivoContenido) || !Files.isRegularFile(rutaArchivoContenido) || !Files.isReadable(rutaArchivoContenido)){
@@ -41,7 +43,17 @@ public class Main {
         try(Stream<String> lineas = Files.lines(rutaArchivoContenido)){
             lineas.map(patron::matcher)
                     .filter(Matcher::find)
-                    .forEach();
+                    .forEach(m -> {
+                        String nombre = m.group("nombre");
+                        String apellido1 = m.group("apellido1");
+                        String apellido2 = m.group("apellido2");
+                        String curso = m.group("curso");
+                        try {
+                            Files.createDirectories(Path.of("src/ejercicio11/salida",curso,apellido1+apellido2+nombre));
+                        } catch (IOException e) {
+                            System.out.printf("%s \n", e.getMessage());
+                        }
+                    });
         }catch (IOException e){
             System.out.printf("%s \n",e.getMessage());
         }
